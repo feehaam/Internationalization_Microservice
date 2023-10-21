0af70e6d246d3500adf891fbd90d86e7015a3880
shartaz-bjit <shartaz.yeasar@bjitacademy.com>
2023-10-21 08:25:34 +0600

    Init

---
+++ src/test/java/com/ladder/i18n/I18NApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladder.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18NApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
